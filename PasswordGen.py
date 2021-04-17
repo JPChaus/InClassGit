716ce831346e4e82afbed27a24dbbe32d9623cd2
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-04-16 22:38:09 -0400

    added password generation and printing

--- PasswordGen.py
+++ PasswordGen.py
@@ -1,1 +1,6 @@
+import string
+import random
+
 num = (int)(input("Enter a positive numer: "))
+characters = string.ascii_letters + string.digits
+print(''.join(random.choice(characters) for i in range(num)))
