0356009d38a059e1839f875250f0dbf81973826a
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-04-16 22:30:52 -0400

    cleaned up divisor

--- divisor.py
+++ divisor.py
@@ -1,7 +1,9 @@
 num = (int)(input("Enter a positive numer: "))
 numList = []
-for i in range(1, num + 1):
-    if(num % i == 0):
-        numList.append(i)
 
-print(*numList)
+def findAndPrint(num, numList)
+    for i in range(1, num + 1):
+        if(num % i == 0):
+            numList.append(i)
+
+    print(*numList)
