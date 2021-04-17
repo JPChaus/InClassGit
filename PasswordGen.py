38815484d31545ff80bc949b312cb64a1d327cb3
JPChaus <48576328+JPChaus@users.noreply.github.com>
2021-04-16 22:38:54 -0400

    cleaned up password gen

--- PasswordGen.py
+++ PasswordGen.py
@@ -2,5 +2,7 @@ import string
 import random
 
 num = (int)(input("Enter a positive numer: "))
-characters = string.ascii_letters + string.digits
-print(''.join(random.choice(characters) for i in range(num)))
+
+def passwordGeneration(num):
+    characters = string.ascii_letters + string.digits
+    print(''.join(random.choice(characters) for i in range(num)))
